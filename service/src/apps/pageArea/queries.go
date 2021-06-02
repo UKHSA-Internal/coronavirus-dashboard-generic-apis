efeea831fbb0e6f575eb03dd3ecc125ba80408a7
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-02 13:59:10 +0100

    Adds additional queries to handle "LA" area type

--- service/src/apps/pageArea/queries.go
+++ service/src/apps/pageArea/queries.go
@@ -1,10 +1,19 @@
 package pageArea
 
 const query = `
-SELECT area_type AS "areaType", area_name AS "areaName", area_code AS "areaCode"
-FROM covid19.page AS pg
-	JOIN covid19.page_area_reference AS par ON par.category_id = pg.id
-	JOIN covid19.area_reference AS ar ON ar.id = par.area_id
-WHERE LOWER(pg.title) = $1`
+	SELECT MIN(ap.priority), ar.area_type, area_name, area_code
+	FROM covid19.page AS pg
+		JOIN covid19.page_area_reference AS par ON par.category_id = pg.id
+		JOIN covid19.area_reference 	 AS ar  ON ar.id = par.area_id
+		JOIN covid19.area_priorities 	 AS ap  ON ap.area_type = ar.area_type
+	WHERE LOWER(pg.title) = $1`
 
-const areaTypeFilter = ` AND ar.area_type = $2`
+const areaTypeFilter = ` AND ar.area_type = %s`
+
+const queryWrapper = `
+SELECT area_type AS "areaType", area_name AS "areaName", area_code AS "areaCode"
+FROM (%s) AS d`
+
+const queryExtras = `
+	GROUP BY ar.area_type, area_code, area_name
+`
