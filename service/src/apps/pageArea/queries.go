77b4cd758b943c2472d95e79fb5ec57e6266fd06
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-05 10:15:37 +0100

    orders by area name + removes duplicates - fixes #1

--- service/src/apps/pageArea/queries.go
+++ service/src/apps/pageArea/queries.go
@@ -1,7 +1,7 @@
 package pageArea
 
 const query = `
-	SELECT MIN(ap.priority), ar.area_type, area_name, area_code
+	SELECT MIN(ap.priority), MIN(ar.area_type) AS area_type, area_name, area_code
 	FROM covid19.page AS pg
 		JOIN covid19.page_area_reference AS par ON par.category_id = pg.id
 		JOIN covid19.area_reference 	 AS ar  ON ar.id = par.area_id
@@ -12,8 +12,9 @@ const areaTypeFilter = ` AND ar.area_type = %s`
 
 const queryWrapper = `
 SELECT area_type AS "areaType", area_name AS "areaName", area_code AS "areaCode"
-FROM (%s) AS d`
+FROM (%s) AS d
+ORDER BY area_name`
 
 const queryExtras = `
-	GROUP BY ar.area_type, area_code, area_name
+	GROUP BY area_code, area_name
 `
