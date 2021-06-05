e2d02afb66a9b1bd1cf406b2f379af874189369a
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-05 13:55:33 +0100

    integrates metric name

--- service/src/apps/soa/queries.go
+++ service/src/apps/soa/queries.go
@@ -2,18 +2,17 @@ package soa
 
 const query = `
 SELECT
-	area_code AS "areaCode",
-	area_name AS "areaName",
-	area_type AS "areaType",
+	area_code 		 AS "areaCode",
+	area_name 		 AS "areaName",
+	area_type 		 AS "areaType",
 	DATE(date)::TEXT AS "date",
-	(payload -> 'rollingSum') AS "rollingSum",
-	(payload -> 'rollingRate') AS "rollingRate",
-	(payload -> 'change') AS "change",
-	(payload -> 'direction') AS "direction",
-	(payload -> 'changePercentage') AS "changePercentage"
+	metric,
+	payload
 FROM %s AS ts
-	JOIN covid19.area_reference AS ar ON ts.area_id = ar.id
+	JOIN covid19.area_reference   AS ar ON ts.area_id = ar.id
+	JOIN covid19.metric_reference AS mr ON ts.metric_id = mr.id
 WHERE area_code = $1
+  AND metric = $2
   AND date = ( SELECT MAX(date) FROM %s )
 `
 
