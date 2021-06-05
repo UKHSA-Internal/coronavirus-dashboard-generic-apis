2825f3a76e507eaf2d2eed766b2b53463e00dbd3
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-05 20:26:37 +0100

    adds date query param to SOA pattern

--- service/src/api/url_patterns.go
+++ service/src/api/url_patterns.go
@@ -12,31 +12,37 @@ var urlPatterns = []routeEntry{
 	{
 		"healthcheck",
 		`/generic/healthcheck`,
+		[]string{},
 		healthcheck.Handler,
 	},
 	{
 		"code",
 		`/generic/code/{area_type:[a-zA-Z]{4,10}}/{area_code:[a-zA-Z0-9+%\s]{3,12}}`,
+		[]string{},
 		code.Handler,
 	},
 	{
 		"soa",
 		`/generic/soa/{area_type:[a-zA-Z]{4,10}}/{area_code:[a-zA-Z0-9]{3,10}}/{metric:[a-zA-Z2860]{5,120}}`,
+		[]string{"date", `200\d-[01]\d-[0123]\d`},
 		soa.Handler,
 	},
 	{
 		"area",
 		`/generic/area/{area_type:[a-zA-Z]{4,10}}`,
+		[]string{},
 		areaByType.Handler,
 	},
 	{
 		"page_areas",
 		`/generic/page_areas/{page:[a-zA-Z]{3,12}}`,
+		[]string{},
 		pageArea.Handler,
 	},
 	{
 		"page_areas_with_type",
 		`/generic/page_areas/{page:[a-zA-Z]{3,12}}/{area_type:[a-zA-Z]{2,12}}`,
+		[]string{},
 		pageArea.Handler,
 	},
 } // routes
