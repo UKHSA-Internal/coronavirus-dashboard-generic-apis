8253c95959a306d53b827f1d9acaf9e00a1cb7c4
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-05 23:25:06 +0100

    minor correction

--- service/src/apps/code/queries.go
+++ service/src/apps/code/queries.go
@@ -9,7 +9,7 @@ FROM covid19.area_reference AS ar
 WHERE id IN (
 	  	 SELECT parent_id
 	  	 FROM covid19.area_reference AS ar2
-		   JOIN covid19.area_relation_se AS pl2 ON pl2.child_id = ar2.id
+		     JOIN covid19.area_relation AS pl2 ON pl2.child_id = ar2.id
 	  	 WHERE area_type = $1
 	  	   AND area_code = $2
 	  )
