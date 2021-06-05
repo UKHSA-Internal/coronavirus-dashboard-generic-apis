12221f393cdf81b26e5dbe7e538ccca7428a10f6
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-05 11:09:06 +0100

    adds instance ID from env vars

--- service/src/insight/insight.go
+++ service/src/insight/insight.go
@@ -10,18 +10,9 @@ var instrumentationKey = os.Getenv("APPINSIGHTS_INSTRUMENTATIONKEY")
 
 func InitialiseInsightClient() appinsights.TelemetryClient {
 
-	var (
-		hostName string
-		err      error
-	)
-
 	insight := appinsights.NewTelemetryClient(instrumentationKey)
-	insight.Context().Tags.Cloud().SetRole(getCloudRoleName())
-	hostName, err = os.Hostname()
-	if err != nil {
-		panic(err)
-	}
-	insight.Context().Tags.Cloud().SetRoleInstance(hostName)
+	insight.Context().Tags.Cloud().SetRole(GetCloudRoleName())
+	insight.Context().Tags.Cloud().SetRoleInstance(GetCloudRoleInstance())
 
 	return insight
 
