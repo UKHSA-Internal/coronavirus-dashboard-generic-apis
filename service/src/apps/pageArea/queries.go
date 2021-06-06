6cc613064c7840c1a7845e8ab09a348749b4ffd4
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-06 11:44:02 +0100

    uses area type based area aggregator

--- service/src/apps/pageArea/queries.go
+++ service/src/apps/pageArea/queries.go
@@ -3,9 +3,9 @@ package pageArea
 const query = `
 	SELECT MIN(ap.priority), MIN(ar.area_type) AS area_type, area_name, area_code
 	FROM covid19.page AS pg
-		JOIN covid19.page_area_reference AS par ON par.category_id = pg.id
-		JOIN covid19.area_reference 	 AS ar  ON ar.id = par.area_id
-		JOIN covid19.area_priorities 	 AS ap  ON ap.area_type = ar.area_type
+		JOIN covid19.page_area_type_reference AS patr ON patr.page = pg.title
+		JOIN covid19.area_reference           AS ar  ON ar.area_type = patr.area_type
+		JOIN covid19.area_priorities 	      AS ap  ON ap.area_type = ar.area_type
 	WHERE LOWER(pg.title) = $1
 	  AND (
 		   LOWER(pg.title) != 'vaccinations'
