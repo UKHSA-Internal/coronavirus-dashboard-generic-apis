e1ee5146179b16db51e33c9eef485e1ca2d141b4
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-09 12:17:55 +0100

    adds patterns for metric availability by area

--- service/src/api/url_patterns.go
+++ service/src/api/url_patterns.go
@@ -4,6 +4,7 @@ import (
 	"generic_apis/apps/area_by_type"
 	"generic_apis/apps/code"
 	"generic_apis/apps/healthcheck"
+	"generic_apis/apps/metric_availability"
 	"generic_apis/apps/page_area"
 	"generic_apis/apps/soa"
 )
@@ -44,5 +45,17 @@ var urlPatterns = []routeEntry{
 		`/generic/page_areas/{page:[a-zA-Z]{3,12}}/{area_type:[a-zA-Z]{2,12}}`,
 		[]string{},
 		page_area.Handler,
+	},
+	{
+		"metric_availability_by_area_type",
+		`/generic/metric_availability/{area_type:[a-zA-Z]{2,12}}`,
+		[]string{"date", `200\d-[01]\d-[0123]\d`},
+		metric_availability.Handler,
+	},
+	{
+		"metric_availability_by_area",
+		`/generic/metric_availability/{area_type:[a-zA-Z]{2,12}}/{area_code:[a-zA-Z0-9]{3,10}}`,
+		[]string{"date", `200\d-[01]\d-[0123]\d`},
+		metric_availability.Handler,
 	},
 } // routes
