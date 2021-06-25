364f29f4fe95e2ca65a327897e3faacdb03d1fc4
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-25 11:56:50 +0100

    accommodates empty assets for metric search query

--- service/src/apps/metric_search/queries.go
+++ service/src/apps/metric_search/queries.go
@@ -4,11 +4,14 @@ const query = `
 SELECT metric,
        MAX(metric_name)           AS metric_name,
        MAX(title)                 AS category,
-       JSON_AGG(tag ORDER BY tag) AS tags
+       CASE
+           WHEN MAX(tag) NOTNULL THEN JSONB_AGG(tag ORDER BY tag)
+           ELSE '[]'::JSONB
+       END AS tags
 FROM covid19.metric_reference AS mr
-	JOIN covid19.page 			  AS pg ON mr.category = pg.id
-	JOIN covid19.metric_tag       AS mt ON mr.metric = mt.metric_id
-	JOIN covid19.tag              AS tg ON mt.tag_id = tg.id
+	LEFT OUTER JOIN covid19.page 		AS pg ON mr.category = pg.id
+	LEFT OUTER JOIN covid19.metric_tag  AS mt ON mr.metric = mt.metric_id
+	LEFT OUTER JOIN covid19.tag         AS tg ON mt.tag_id = tg.id
 WHERE mr.metric ~* ('.*' || $1 || '.*')
   AND mr.released IS TRUE
 GROUP BY metric
