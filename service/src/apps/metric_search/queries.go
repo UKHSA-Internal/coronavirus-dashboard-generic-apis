b5d5e6f393ac7b3e62444dd39d2cda0d4d13b01a
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-06-26 13:01:44 +0100

    adds SQL queries for search by tags or category

--- service/src/apps/metric_search/queries.go
+++ service/src/apps/metric_search/queries.go
@@ -12,12 +12,22 @@ FROM covid19.metric_reference AS mr
 	LEFT OUTER JOIN covid19.page 		AS pg ON mr.category = pg.id
 	LEFT OUTER JOIN covid19.metric_tag  AS mt ON mr.metric = mt.metric_id
 	LEFT OUTER JOIN covid19.tag         AS tg ON mt.tag_id = tg.id
-WHERE (
-		   mr.metric ~* ('.*' || REGEXP_REPLACE($1, '\s+', '', 'g') || '.*')
-	    OR mr.metric_name ILIKE ('%' || REGEXP_REPLACE('cases rolling rate', '\s+', '%', 'g') || '%')
-      )
-  AND mr.released IS TRUE
+WHERE mr.released IS TRUE %s
 GROUP BY metric
 ORDER BY metric`
 
 const metricTemplate = "%s"
+
+const searchFilter = ` AND (
+	   mr.metric ~* ('.*' || REGEXP_REPLACE($%d, '\s+', '', 'g') || '.*')
+	OR mr.metric_name ILIKE ('%%' || REGEXP_REPLACE($%d, '\s+', '%%', 'g') || '%%')
+  )`
+
+const categoryFilter = " AND LOWER(pg.title) = $%d"
+
+const tagsFilter = ` AND mt.metric_id IN (
+    SELECT metric_id
+    FROM covid19.tag AS tg
+        LEFT JOIN covid19.metric_tag AS mt ON mt.tag_id = tg.id
+    WHERE tag = ANY ($%d::VARCHAR[])
+  )`
