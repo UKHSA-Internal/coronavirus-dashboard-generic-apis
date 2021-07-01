c2d01512f3ec9df95d646f2111a0631be885e19b
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-07-01 10:31:53 +0100

    simplifies query for latest record

--- service/src/apps/soa/queries.go
+++ service/src/apps/soa/queries.go
@@ -2,22 +2,21 @@ package soa
 
 const query = `
 SELECT
-	area_code 		 AS "areaCode",
-	area_name 		 AS "areaName",
-	area_type 		 AS "areaType",
-	DATE(date)::TEXT AS "date",
+	area_code    	AS "areaCode",
+	area_name    	AS "areaName",
+	area_type    	AS "areaType",
+	date::TEXT    	AS "date",
 	metric,
 	payload
 FROM %s AS ts
 	JOIN covid19.area_reference   AS ar ON ts.area_id = ar.id
 	JOIN covid19.metric_reference AS mr ON ts.metric_id = mr.id
 WHERE area_code = $1
-  AND metric = $2
-  %s
+  AND metric = $2 %s
+ORDER BY date DESC
+LIMIT 1
 `
 
 const queryTable = "covid19.time_series_p%s_%s"
-
-const latestDate = "AND date = ( SELECT MAX(date) FROM %s )"
 
 const definedDate = "AND date = $3"
