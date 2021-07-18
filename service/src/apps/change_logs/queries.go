412888c626f9de6a501e7208961101b69c6505cc
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-07-18 12:27:53 +0100

    updates search query + housekeeping

--- service/src/apps/change_logs/queries.go
+++ service/src/apps/change_logs/queries.go
@@ -1,14 +1,23 @@
-package change_log
+package change_logs
+
+const (
+	QueryToken      = "{token_id}"
+	PaginationToken = "{#pagination#}"
+	FiltersToken    = "{filters}"
+)
 
 const simpleQuery = `
-SELECT cl.date,
+SELECT cl.date::TEXT,
        cl.heading,
        MAX(cl.body)    AS body,
        MAX(cl.details) AS details,
        cl.high_priority,
 	   cl.display_banner,
        t.tag 		   AS type,
-       MAX(p.title)    AS page_title,
+       JSONB_BUILD_OBJECT(
+			'title', MAX(p.title),
+       		'uri', MAX(p.uri)
+	   )               AS page,
        JSONB_AGG(
            jsonb_build_object(
                'metric', mr.metric,
@@ -24,14 +33,13 @@ FROM covid19.change_log AS cl
 {filters}
 GROUP BY cl.date, cl.heading, t.tag, cl.high_priority, cl.display_banner
 ORDER BY cl.date DESC
-{pagination};
+{#pagination#};
 `
 
 const searchQuery = `
 SELECT *
 FROM (
-         SELECT cl.date,
-                MAX(cl.details)  AS details,
+         SELECT cl.date::TEXT,
                 cl.high_priority,
                 cl.display_banner,
                 t.tag           AS type,
@@ -42,31 +50,30 @@ FROM (
                         'metric_name', mr.metric_name
                     )
                 )               AS metrics,
-                MAX(p.title)    As page_title,
-                ts_headline(
-                    cl.heading,
-                    plainto_tsquery('english', ${token_id})
-                )::TEXT         AS heading,
-                ts_headline(
-                    MAX(cl.body),
-                    plainto_tsquery('english', ${token_id})
-                )::TEXT         AS body,
+                JSONB_BUILD_OBJECT(
+	         		'title', MAX(p.title),
+					'uri', MAX(p.uri)
+	            )               AS page,
+                cl.heading,
+                MAX(cl.body)    AS body,
                 ROUND(
-                    ts_rank(to_tsvector('english', MAX(cl.body)), plainto_tsquery('english', ${token_id}))::NUMERIC +
-                    ts_rank(to_tsvector('english', cl.heading),  plainto_tsquery('english', ${token_id}))::NUMERIC * 2,
+                    ts_rank(to_tsvector('english'::REGCONFIG, MAX(cl.body)), plainto_tsquery('english', ${token_id}))::NUMERIC +
+                    ts_rank(to_tsvector('english'::REGCONFIG, cl.heading),  plainto_tsquery('english', ${token_id}))::NUMERIC * 2,
                     5
-                )               AS rank
+                )::FLOAT        AS rank
          FROM covid19.change_log AS cl
            LEFT JOIN covid19.tag                  AS t ON t.id = cl.type_id
            LEFT JOIN covid19.change_log_to_metric AS cltm ON cltm.log_id = cl.id
            LEFT JOIN covid19.metric_reference     AS mr ON mr.metric = cltm.metric_id
            LEFT JOIN covid19.change_log_to_page   AS cltp ON cltp.log_id = cl.id
            LEFT JOIN covid19.page                 AS p ON p.id = cltp.page_id
+		 WHERE to_tsvector('english'::REGCONFIG, cl.body) @@ plainto_tsquery('english', ${token_id})
+            OR to_tsvector('english'::REGCONFIG, cl.heading) @@ plainto_tsquery('english', ${token_id})
          GROUP BY cl.heading, cl.date, t.tag, cl.high_priority, cl.display_banner
     ) AS df
     {filters}
 ORDER BY rank DESC, df.date DESC
-{pagination};
+{#pagination#};
 `
 
 // const recordMonths = `
@@ -80,6 +87,7 @@ const paginationQuery = "LIMIT 20 OFFSET %d"
 
 var queryParamFilters = map[string]string{
 	"search": `rank > 0.01`,
-	"type":   `t.tag = ${token_id}`,
-	"date":   `date BETWEEN ${token_id}::DATE AND ${token_id}::DATE + INTERVAL '1 month'`,
+	"title":  `LOWER(p.title) = LOWER(${token_id})`,
+	"type":   `LOWER(p.tag) = LOWER(${token_id})`,
+	"date":   `date::DATE BETWEEN ${token_id}::DATE AND ${token_id}::DATE + INTERVAL '1 month'`,
 }
