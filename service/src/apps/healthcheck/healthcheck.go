45da6c90d30fe028baf748df133eb9d5a540597d
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-07-19 11:28:42 +0100

    changes observations

--- service/src/apps/healthcheck/healthcheck.go
+++ service/src/apps/healthcheck/healthcheck.go
@@ -2,65 +2,43 @@ package healthcheck
 
 import (
 	"net/http"
+	"time"
 
-	"generic_apis/apps/utils"
-	"generic_apis/db"
-	"generic_apis/insight"
-	"github.com/microsoft/ApplicationInsights-Go/appinsights"
+	hc "github.com/etherlabsio/healthcheck/v2"
+	"github.com/etherlabsio/healthcheck/v2/checkers"
 )
 
-type handler struct {
-	db          *db.Config
-	traceparent string
-}
+// type handler struct {
+// 	db          *db.Config
+// 	traceparent string
+// }
+//
+// func (conf *handler) fromDatabase() ([]byte, error) {
+//
+// 	payload := &db.Payload{
+// 		Query:         healthCheckQuery,
+// 		Args:          []interface{}{},
+// 		OperationData: insight.GetOperationData(conf.traceparent),
+// 	}
+//
+// 	_, err := conf.db.FetchAll(payload)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	data := map[string]string{
+// 		"database": "healthy",
+// 	}
+//
+// 	return utils.JSONMarshal(data)
+//
+// } // FromDatabase
 
-func (conf *handler) fromDatabase() ([]byte, error) {
+func Handler() http.Handler {
 
-	payload := &db.Payload{
-		Query:         healthCheckQuery,
-		Args:          []interface{}{},
-		OperationData: insight.GetOperationData(conf.traceparent),
-	}
-
-	_, err := conf.db.FetchAll(payload)
-	if err != nil {
-		return nil, err
-	}
-
-	data := map[string]string{
-		"database": "healthy",
-	}
-
-	return utils.JSONMarshal(data)
-
-} // FromDatabase
-
-func Handler(insight appinsights.TelemetryClient) func(w http.ResponseWriter, r *http.Request) {
-
-	conf := &handler{}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		var err error
-
-		conf.traceparent = r.Header.Get("traceparent")
-
-		conf.db, err = db.Connect(insight)
-		if err != nil {
-			panic(err)
-		}
-		defer conf.db.CloseConnection()
-
-		response, err := conf.fromDatabase()
-		if err != nil {
-			http.Error(w, "db connection failed", http.StatusInternalServerError)
-			return
-		}
-
-		if _, err = w.Write(response); err != nil {
-			panic(err)
-		}
-
-	}
+	return hc.Handler(
+		hc.WithTimeout(5*time.Second),
+		hc.WithObserver("diskspace", checkers.DiskSpace("/var/log", 90)),
+	)
 
 } // queryByCode
