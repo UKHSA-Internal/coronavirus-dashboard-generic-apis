1abccb16528274f76d181058d528b6dd76ff5599
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-07-20 11:10:37 +0100

    runs assertions w/ t.Run

--- service/src/assert/assertions.go
+++ service/src/assert/assertions.go
@@ -9,73 +9,84 @@ import (
 
 func JsonObjResponseMatchExpected(t *testing.T, expected interface{}, jsonResponse []byte) {
 
-	response := make(map[string]interface{})
-	if err := json.Unmarshal(jsonResponse, &response); err != nil {
-		t.Error(err)
-	}
+	t.Run("JsonObjResponseMatchExpected", func(t *testing.T) {
+		response := make(map[string]interface{})
+		if err := json.Unmarshal(jsonResponse, &response); err != nil {
+			t.Error(err)
+		}
 
-	if !cmp.Equal(response, expected) {
-		difference := cmp.Diff(response, expected)
-		t.Errorf(
-			"response does not match the expected output\nExpected: %v\nActual: %v\nDifference: %v",
-			expected, response, difference)
-	}
+		if !cmp.Equal(response, expected) {
+			difference := cmp.Diff(response, expected)
+			t.Errorf(
+				"response does not match the expected output\nExpected: %v\nActual: %v\nDifference: %v",
+				expected, response, difference)
+		}
+	})
 
 } // JsonResponseMatchExpected
 
 func JsonArrResponseMatchExpected(t *testing.T, expected interface{}, jsonResponse []byte) {
 
-	response := make([]map[string]interface{}, 0)
-	if err := json.Unmarshal(jsonResponse, &response); err != nil {
-		t.Error(err)
-	}
+	t.Run("JsonArrResponseMatchExpected", func(t *testing.T) {
+		response := make([]map[string]interface{}, 0)
+		if err := json.Unmarshal(jsonResponse, &response); err != nil {
+			t.Error(err)
+		}
 
-	if !cmp.Equal(response, expected) {
-		difference := cmp.Diff(response, expected)
-		t.Errorf(
-			"response does not match the expected output\nExpected: %v\nActual: %v\nDifference: %v",
-			expected, response, difference)
-	}
+		if !cmp.Equal(response, expected) {
+			difference := cmp.Diff(response, expected)
+			t.Errorf(
+				"response does not match the expected output\nExpected: %v\nActual: %v\nDifference: %v",
+				expected, response, difference)
+		}
+	})
 
 } // JsonResponseMatchExpected
 
 func JsonArrResponseContains(t *testing.T, expected interface{}, jsonResponse []byte) {
 
-	response := make([]map[string]interface{}, 0)
-	if err := json.Unmarshal(jsonResponse, &response); err != nil {
-		t.Error(err)
-	}
+	t.Run("JsonArrResponseContains", func(t *testing.T) {
 
-	lenExpected := len(expected.([]map[string]interface{}))
-	lenDetected := 0
+		response := make([]map[string]interface{}, 0)
+		if err := json.Unmarshal(jsonResponse, &response); err != nil {
+			t.Error(err)
+		}
 
-	for _, expectedItem := range expected.([]map[string]interface{}) {
-		for _, responseItem := range response {
-			if cmp.Equal(expectedItem, responseItem) {
-				lenDetected += 1
-				break
+		lenExpected := len(expected.([]map[string]interface{}))
+		lenDetected := 0
+
+		for _, expectedItem := range expected.([]map[string]interface{}) {
+			for _, responseItem := range response {
+				if cmp.Equal(expectedItem, responseItem) {
+					lenDetected += 1
+					break
+				}
 			}
 		}
-	}
 
-	if lenExpected != lenDetected {
-		t.Errorf("Expected to find %d items in the response, found %d instead", lenExpected, lenDetected)
-	}
+		if lenExpected != lenDetected {
+			t.Errorf("Expected to find %d items in the response, found %d instead", lenExpected, lenDetected)
+		}
+	})
 
 } // JsonResponseMatchExpected
 
 func Equal(t *testing.T, topic string, expected, actual interface{}) {
 
-	if expected != actual {
-		t.Errorf("[%v] Expected response code <%v>. Got <%v>\n", topic, expected, actual)
-	}
+	t.Run(topic, func(t *testing.T) {
+		if expected != actual {
+			t.Errorf("[%v] Expected response code <%v>. Got <%v>\n", topic, expected, actual)
+		}
+	})
 
 } // Equal
 
-func AssertIntGreater(t *testing.T, topic string, expected, actual int) {
+func IntGreater(t *testing.T, topic string, expected, actual int) {
 
-	if expected <= actual {
-		t.Errorf("[%v] Expected response code <%v>. Got <%v>\n", topic, expected, actual)
-	}
+	t.Run(topic, func(t *testing.T) {
+		if expected <= actual {
+			t.Errorf("[%v] Expected response code <%v>. Got <%v>\n", topic, expected, actual)
+		}
+	})
 
-} // AssertGreater
+} // IntGreater
