af884844fe9ed3257e5f6d11e8f78d9df8d15014
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-08-10 13:00:11 +0100

    updates CSP default-src and style-src attrs

--- service/src/middleware/headers_middleware.go
+++ service/src/middleware/headers_middleware.go
@@ -14,9 +14,10 @@ var headers = map[string]string{
 	"x-content-type-options":    "nosniff",
 	"x-xss-protection":          "1; mode=block",
 	"referrer-policy":           "origin-when-cross-origin, strict-origin-when-cross-origin",
-	"content-security-policy":   "default-src 'none'; style-src 'self' 'unsafe-inline'",
-	"x-phe-media-type":          "PHE-COVID19.v1",
-	"phe-server-loc":            os.Getenv("SERVER_LOCATION"),
+	"content-security-policy": "default-src 'self' coronavirus.data.gov.uk *.coronavirus.data.gov.uk; " +
+		"style-src 'self' 'unsafe-inline' coronavirus.data.gov.uk *.coronavirus.data.gov.uk",
+	"x-phe-media-type": "PHE-COVID19.v1",
+	"phe-server-loc":   os.Getenv("SERVER_LOCATION"),
 }
 
 func HeadersMiddleware(next http.Handler) http.Handler {
