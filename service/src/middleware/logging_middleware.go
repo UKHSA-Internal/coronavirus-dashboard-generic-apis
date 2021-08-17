cf96e295e56ae99651b39ee41e0582e815e10b91
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-08-17 11:18:07 +0100

    adds logging middleware

---
+++ service/src/middleware/logging_middleware.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func LogRequest(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		log.Printf(
+			"%s %s %s",
+			r.Method,
+			r.RequestURI,
+			time.Since(start),
+		)
+	})
+
+} // LogRequest
