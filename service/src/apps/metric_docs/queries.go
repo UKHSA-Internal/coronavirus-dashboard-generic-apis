cdacd4e575d335222b25463b6cc3588fd7b65b60
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-08-26 12:29:05 +0100

    updates linebreak chars

--- service/src/apps/metric_docs/queries.go
+++ service/src/apps/metric_docs/queries.go
@@ -4,7 +4,7 @@ const mainQuery = `
 SELECT MAX(metric_name) AS metric_name,
        metric,
 	   LOWER(asset_type) AS asset_type,
-       STRING_AGG(ma.body, '\r\n\r\n' ORDER BY "order") AS body,
+       STRING_AGG(ma.body, ' \n\n' ORDER BY "order") AS body,
        MAX(ma.last_modified)::TIMESTAMP WITHOUT TIME ZONE AS last_modified
 FROM covid19.metric_reference AS mr
   JOIN covid19.metric_asset_to_metric AS matm ON mr.metric = matm.metric_id
