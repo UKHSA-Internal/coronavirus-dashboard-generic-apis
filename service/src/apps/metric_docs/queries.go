595cc274f33ba8164631df1c18b9fd2447479339
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-08-26 19:15:46 +0100

    remove duplicates from log

--- service/src/apps/metric_docs/queries.go
+++ service/src/apps/metric_docs/queries.go
@@ -6,7 +6,7 @@ SELECT MAX(metric_name) AS metric_name,
 	   LOWER(asset_type) AS asset_type,
        STRING_AGG(ma.body, E'\n\n' ORDER BY "order") AS body,
        MAX(ma.last_modified)::TIMESTAMP WITHOUT TIME ZONE AS last_modified,
-       JSONB_AGG(cl.payload::JSONB) AS logs
+       JSONB_AGG(DISTINCT cl.payload::JSONB) AS logs
 FROM covid19.metric_reference AS mr
   LEFT JOIN covid19.metric_asset_to_metric AS matm ON mr.metric = matm.metric_id
   LEFT JOIN covid19.metric_asset           AS ma   ON ma.id = matm.asset_id
@@ -22,6 +22,7 @@ FROM covid19.metric_reference AS mr
 			 ) AS payload
       FROM covid19.change_log AS cl_inner
       LEFT JOIN covid19.tag AS t ON t.id = cl_inner.type_id
+      ORDER BY date DESC
     ) AS cl   ON cltm.log_id = cl.id
 WHERE mr.metric ILIKE ${metric_token}
 GROUP BY metric, asset_type;
