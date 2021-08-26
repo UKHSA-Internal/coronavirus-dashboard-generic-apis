275256edc9b0cee7363847307d98450ea984e085
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-08-26 14:05:14 +0100

    adds logs to query

--- service/src/apps/metric_docs/queries.go
+++ service/src/apps/metric_docs/queries.go
@@ -5,10 +5,22 @@ SELECT MAX(metric_name) AS metric_name,
        metric,
 	   LOWER(asset_type) AS asset_type,
        STRING_AGG(ma.body, E'\n\n' ORDER BY "order") AS body,
-       MAX(ma.last_modified)::TIMESTAMP WITHOUT TIME ZONE AS last_modified
+       MAX(ma.last_modified)::TIMESTAMP WITHOUT TIME ZONE AS last_modified,
+       JSONB_AGG(cl.payload::JSONB) AS logs
 FROM covid19.metric_reference AS mr
-  JOIN covid19.metric_asset_to_metric AS matm ON mr.metric = matm.metric_id
-  JOIN covid19.metric_asset           AS ma   ON ma.id = matm.asset_id
-WHERE LOWER(mr.metric) = LOWER(${metric_token})
+  LEFT JOIN covid19.metric_asset_to_metric AS matm ON mr.metric = matm.metric_id
+  LEFT JOIN covid19.metric_asset           AS ma   ON ma.id = matm.asset_id
+  LEFT JOIN covid19.change_log_to_metric   AS cltm ON mr.metric = cltm.metric_id
+  LEFT JOIN (
+      SELECT id,
+			 JSONB_BUILD_OBJECT(
+			 	 'id', id::TEXT,
+				 'date', date::TEXT,
+				 'expiry', expiry::TEXT,
+				 'heading', heading
+			 ) AS payload
+      FROM covid19.change_log
+    ) AS cl   ON cltm.log_id = cl.id
+WHERE mr.metric ILIKE ${metric_token}
 GROUP BY metric, asset_type;
 `
