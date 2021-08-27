1239373d34fadfd8783d89a55739e2a227f57188
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-08-27 13:57:53 +0100

    adds category and tags to query

--- service/src/apps/metric_docs/queries.go
+++ service/src/apps/metric_docs/queries.go
@@ -6,7 +6,9 @@ SELECT MAX(metric_name) AS metric_name,
        asset_type,
        STRING_AGG(body, E'\n\n' ORDER BY "order") AS body,
        MAX(last_modified) AS last_modified,
-       MAX(logs::TEXT)::JSONB AS logs
+       MAX(logs::TEXT)::JSONB AS logs,
+       MAX(tags) AS tags,
+       MAX(category) AS category
 FROM (
 	SELECT DISTINCT ma.body AS body,
            MAX(metric_name) AS metric_name,
@@ -14,7 +16,9 @@ FROM (
            LOWER(asset_type) AS asset_type,
            MAX("order") AS "order",
            MAX(ma.last_modified)::TIMESTAMP WITHOUT TIME ZONE AS last_modified,
-           JSONB_AGG(DISTINCT cl.payload::JSONB) AS logs
+           JSONB_AGG(DISTINCT cl.payload::JSONB) AS logs,
+	       STRING_AGG(DISTINCT tg.tag, ',') AS tags,
+           MAX(pg.title) AS category
 	FROM covid19.metric_reference AS mr
 	  LEFT JOIN covid19.metric_asset_to_metric AS matm ON mr.metric = matm.metric_id
 	  LEFT JOIN covid19.metric_asset           AS ma   ON ma.id = matm.asset_id
@@ -30,8 +34,17 @@ FROM (
 				 ) AS payload
 		  FROM covid19.change_log AS cl_inner
 		  LEFT JOIN covid19.tag AS t ON t.id = cl_inner.type_id
+		  WHERE
+			  cl_inner.date <= (
+				  SELECT MAX(timestamp)::DATE
+				  FROM covid19.release_reference AS rr
+				  WHERE rr.released IS TRUE
+			  )
 		  ORDER BY date DESC
 	  ) AS cl ON cltm.log_id = cl.id
+	LEFT JOIN covid19.metric_tag AS mtg ON mr.metric = mtg.metric_id
+	LEFT JOIN covid19.tag AS tg ON mtg.tag_id = tg.id
+	LEFT JOIN covid19.page AS pg ON mr.category = pg.id
 	WHERE mr.metric ILIKE ${metric_token}
 	GROUP BY metric, asset_type, body
 ) AS dd
