cb9bfacae8801d070cabab06c24846439201e47c
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-09-24 13:40:56 +0100

    updates query to use CTE

--- service/src/apps/metric_areas/queries.go
+++ service/src/apps/metric_areas/queries.go
@@ -1,12 +1,16 @@
 package metric_areas
 
 const mainQuery = `
+WITH latest_release AS (
+    SELECT MAX(timestamp)::DATE AS date
+    FROM covid19.release_reference
+)
 SELECT area_type, last_update
 FROM (
          SELECT area_type, MAX(date)::DATE::TEXT AS last_update
          FROM covid19.time_series_p${partition_date}_other AS ts
                   JOIN covid19.area_reference AS ar ON ar.id = ts.area_id
-         WHERE date > (NOW() - INTERVAL '30 days')
+         WHERE date > ((SELECT MAX(date) FROM latest_release) - INTERVAL '30 days')
            AND metric_id IN (SELECT id
                              FROM covid19.metric_reference AS mr
                              WHERE metric ILIKE ${metric_token}
@@ -17,7 +21,7 @@ FROM (
              SELECT area_type, MAX(date)::DATE::TEXT AS last_update
              FROM covid19.time_series_p${partition_date}_utla AS ts
                       JOIN covid19.area_reference AS ar ON ar.id = ts.area_id
-             WHERE date > (NOW() - INTERVAL '30 days')
+             WHERE date > ((SELECT MAX(date) FROM latest_release) - INTERVAL '30 days')
                AND metric_id IN (SELECT id
                                  FROM covid19.metric_reference AS mr
                                  WHERE metric ILIKE ${metric_token}
@@ -29,7 +33,7 @@ FROM (
              SELECT area_type, MAX(date)::DATE::TEXT AS last_update
              FROM covid19.time_series_p${partition_date}_ltla AS ts
                       JOIN covid19.area_reference AS ar ON ar.id = ts.area_id
-             WHERE date > (NOW() - INTERVAL '30 days')
+             WHERE date > ((SELECT MAX(date) FROM latest_release) - INTERVAL '30 days')
                AND metric_id IN (SELECT id
                                  FROM covid19.metric_reference AS mr
                                  WHERE metric ILIKE ${metric_token}
@@ -41,7 +45,7 @@ FROM (
              SELECT area_type, MAX(date)::DATE::TEXT AS last_update
              FROM covid19.time_series_p${partition_date}_nhstrust AS ts
                       JOIN covid19.area_reference AS ar ON ar.id = ts.area_id
-             WHERE date > (NOW() - INTERVAL '30 days')
+             WHERE date > ((SELECT MAX(date) FROM latest_release) - INTERVAL '30 days')
                AND metric_id IN (SELECT id
                                  FROM covid19.metric_reference AS mr
                                  WHERE metric ILIKE ${metric_token}
@@ -53,7 +57,7 @@ FROM (
              SELECT area_type, MAX(date)::DATE::TEXT AS last_update
              FROM covid19.time_series_p${partition_date}_msoa AS ts
                       JOIN covid19.area_reference AS ar ON ar.id = ts.area_id
-             WHERE date > (NOW() - INTERVAL '30 days')
+             WHERE date > ((SELECT MAX(date) FROM latest_release) - INTERVAL '30 days')
                AND metric_id IN (SELECT id
                                  FROM covid19.metric_reference AS mr
                                  WHERE metric ILIKE ${metric_token}
