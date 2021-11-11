0eb5d8648f03ceb3e4baf54d57887945e7620777
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-11 16:48:36 +0000

    adds generic timestamp query

--- service/src/apps/utils/queries.go
+++ service/src/apps/utils/queries.go
@@ -1,9 +1,15 @@
 package utils
 
-const timestampQuery = `
-SELECT DATE(MAX(timestamp))::TEXT AS date
+const processTimestampQuery = `
+SELECT MAX(timestamp)::DATE::TEXT AS date
 FROM covid19.release_reference AS rr
 	JOIN covid19.release_category AS rc ON rc.release_id = rr.id
 WHERE released IS TRUE
   AND process_name = $1
+`
+
+const timestampQuery = `
+SELECT MAX(timestamp)::DATE::TEXT AS date
+FROM covid19.release_reference AS rr
+WHERE released IS TRUE
 `
